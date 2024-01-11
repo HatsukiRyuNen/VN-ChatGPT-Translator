97f03a272ee0df24c045e42ba97554bdd1ee7b08
RyuNen <1060940637@qq.com>
2024-01-11 21:55:42 +0800

    修改了config.py的配置“

--- configs.py
+++ configs.py
@@ -8,15 +8,15 @@ all_text = os.listdir(text_dir)
 
 total_accounts = 1  # 总共账号数量
 
-chrome_version = 113  # Chrome的大版本
+chrome_version = 120  # Chrome的大版本
 
 # 请在这里放置session的url。
 urls = {
-    1: 'https://chat.openai.com/c/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    1: 'http://192.168.88.128:8181/c/7d410a79-ab13-42d7-b6f6-e3ba3fd0206b',
 }
 
 # 请在这里放置cookies。
-cookies_mapping = {1: 'cookie_example.json'}
+cookies_mapping = {1: 'cookies.json'}
 
 prompt = 'please translate the following galgame conversations from Japanese to Chinese. ' + \
         'Please preserve escape characters like \\r\\n or \\" and strictly keep the JSON format of the provided content. ' + \
@@ -29,4 +29,4 @@ answer_waiting_time = 10
 
 # the maximum number of lines to input to ChatGPT each time
 # 每次输入ChatGPT的最大行数
-batch_size = 15
+batch_size = 10
